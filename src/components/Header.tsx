0a696d574791e9daa47ca7db23b60e9a5975bee4
Tayo <teeydigba@gmail.com>
2024-02-19 17:52:01 +0100

    completed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const Header = ({ pageName }) => {
   return (
     <div className="w-full bg-white flex items-center justify-between sticky left-0 top-0 h-[70px] px-[30px] pt-[23px] pb-[13px]">
@@ -5,8 +7,21 @@ export const Header = ({ pageName }) => {
         <p className="text-[20px] font-[500]">{pageName}</p>
       </div>
 
-      <div className="flex gap-[20px]">
-        <div className="w-[40px] h-[40px] bg-[#EBFFF7] rounded-[50%] flex justify-center items-center"></div>
+      <div className="flex gap-[10px] items-center">
+        <div className="w-[40px] h-[40px] bg-light-wine rounded-[50%] flex justify-center items-center text-primary-wine">
+          AO
+        </div>
+        <div className="">
+          <p className="text-[14px]">Ayodeji Emmanuel</p>
+          <p className="text-black/60 text-[12px]">ayodejiemmanuel@gmail.com</p>
+        </div>
+        <Image
+          src="/icons/down-chevron.svg"
+          alt="chevron icon"
+          width={20}
+          height={20}
+          className="ml-[14px]"
+        />
       </div>
     </div>
   );
