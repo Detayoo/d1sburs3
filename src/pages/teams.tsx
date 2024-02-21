f97ddaa7f0f4c0532e806b5a7ebae5073660e733
Tayo <teeydigba@gmail.com>
2024-02-21 10:45:40 +0100

    fixed teams page tab font

--- src/pages/teams.tsx
+++ src/pages/teams.tsx
@@ -33,7 +33,7 @@ const Teams = () => {
             onClick={() => setActiveTab(tab)}
             className={`w-[120px] capitalize border-b-[2px] pb-[11px] text-sm text-center self-end cursor-pointer ${
               activeTab === tab
-                ? "border-b-primary-wine text-primary-wine font-[500]"
+                ? "border-b-primary-wine text-primary-wine font-InterTight-Medium"
                 : "border-b-transparent text-[#5D5D5D]"
             }`}
           >
