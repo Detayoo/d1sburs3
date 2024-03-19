82431e2e56276a6b46eb385148e6dfc3789cf146
Tayo <teeydigba@gmail.com>
2024-03-19 10:50:41 +0100

    code cleanup

--- src/utils/AuthenticatedRoute.tsx
+++ src/utils/AuthenticatedRoute.tsx
@@ -1,13 +1,10 @@
 import Router from "next/router";
 import { AppProps } from "next/app";
-import { useIdleTimer } from "react-idle-timer";
 
 import { useAuth } from "@/contexts";
 import { IdleTimer } from "@/components";
 
 export const AuthenticatedRoute = (Component: any) => {
-  const logout = () => localStorage.clear();
-
   // eslint-disable-next-line react/display-name
   return (props: AppProps) => {
     // eslint-disable-next-line react/display-name, react-hooks/rules-of-hooks
