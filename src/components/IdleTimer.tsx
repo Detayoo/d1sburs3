3733d313512846c430a13ae6ccdd1887506e80ad
Tayo <teeydigba@gmail.com>
2024-03-19 10:41:37 +0100

    added correct message

--- src/components/IdleTimer.tsx
+++ src/components/IdleTimer.tsx
@@ -1,29 +1,24 @@
-import React, { useState } from "react";
 import { useIdleTimer } from "react-idle-timer";
+import { toast } from "react-toastify";
 
 import { useAuth } from "@/contexts";
-import { toast } from "react-toastify";
 
 export const IdleTimer = () => {
   const { logout } = useAuth();
-  const [isIdle, setIdle] = useState(false);
 
   const handleIdle = () => {
-    setIdle(true);
-    console.log("about to fire");
+    toast.error(
+      "You have been inactive for a while, please login to continue using the portal"
+    );
     logout();
   };
-  const timeout = 1 * 1000 * 60;
+  const timeout = 5 * 60 * 1000;
 
-  const idleTimer = useIdleTimer({
+  useIdleTimer({
     timeout,
     onIdle: handleIdle,
     debounce: 500,
   });
 
-  return (
-    idleTimer.getElapsedTime() > timeout / 2 &&
-    isIdle &&
-    toast.error("You have been inactive for a while, please log in")
-  );
+  return null;
 };
